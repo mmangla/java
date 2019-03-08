73094502cf7bd8a9359eb7134cd650f8c88b28a7
mmangla <manoj.mangla@equilend.com>
2019-03-07 21:31:36 -0500

    Add files via upload

---
+++ basictest/src/TestFirst.java
@@ -0,0 +1,12 @@
+public class TestFirst {
+    public static void main(String[] args) {
+        TestFirst testFirst = new TestFirst();
+        testFirst.doFirst();
+    }
+
+    private void doFirst() {
+        System.out.println("Hello");
+    }
+
+
+}
